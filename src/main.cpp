f780a7cbc353909f23752b099e9f24f9c2cdffe8
MOMO <MOMO@localhost.localdomain>
2017-07-15 05:36:21 +0800

    add ffmpeg－3.3.2 lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
 
+#ifdef __cplusplus
+extern "C"
+{
+    #define __STDC_CONSTANT_MACROS
+    #ifndef INT64_C
+    #define INT64_C
+    #define UINT64_C
+    #endif
+    #include <stdint.h>
+
+    #include "libavutil/avutil.h"
+    #include "libavcodec/avcodec.h"
+    #include "libavformat/avformat.h"
+    #include "libavdevice/avdevice.h"
+
+
+    #include "libswscale/swscale.h"
+    #include "libswresample/swresample.h"
+}
+#endif
+
+#include <iostream>
+using namespace std;
+
+
 int main() {
+    av_register_all();	
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
